672ebd1d9ff525d7993a1a8cf53a2c2ec9e79bb5
Henrik Bjørnskov <henrik@bjrnskov.dk>
2011-12-05 10:38:46 +0100

    License

--- DependencyInjection/HBStampieExtension.php
+++ DependencyInjection/HBStampieExtension.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the HBStampieBundle package.
+ *
+ * (c) Henrik Bjornskov <henrik@bjrnskov.dk>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace HB\StampieBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
